823f59a8f0ddd4ff4a8844c4426ac26d3a994cd5
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-09-30 21:17:12 +0300

    feat: finish typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (array == undefined || !array.length) {
+        return 0
+    } else {
+        let sortedArray = array.sort((a, b) => {
+            return a - b;
+        })
 
-exports.min = function min (array) {
-  return 0;
+        return sortedArray[0];
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == undefined || !array.length) {
+        return 0
+    } else {
+        let sortedArray = array.sort((a, b) => {
+            return a - b;
+        }).reverse();
+
+        return sortedArray[0];
+    }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == undefined || !array.length) {
+        return 0
+    } else {
+        let summ = null;
+        array.forEach((it) => {
+            summ += it;
+        })
+
+        return summ / array.length;
+    }
 }
